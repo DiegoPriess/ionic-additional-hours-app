defd5f267cab59419310282ce9a5f6bbe9052b0c
Diego Priess <diegopriess.dev@gmail.com>
2022-12-07 22:19:19 -0300

    first commit

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+
+import { LoginPageRoutingModule } from './login-routing.module';
+
+import { LoginPage } from './login.page';
+
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoginPageRoutingModule,
+    HttpClientModule
+  ],
+  declarations: [LoginPage]
+})
+export class LoginPageModule {}
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+
+  httpOptions: object = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" })
+  }
+
+  apiUrl: string = 'http://localhost:8080';
+  email: string = "";
+  password: string = "";
+
+  constructor(private httpClient: HttpClient, private router: Router, private storage: Storage) {
+  }
+
+  authenticate(){
+     this.httpClient.get(`${this.apiUrl}/user/auth/${this.email}/${this.password}`, this.httpOptions)
+     .pipe(
+       retry(2),
+       catchError(this.handleError)
+      ).subscribe((Response: object) => {
+        this.router.navigate(['/detail']);
+      });
+  }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  };
+
+}
