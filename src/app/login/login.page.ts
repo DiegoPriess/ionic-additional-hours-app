21a11c671b83dc8a44eacc5730e40f7322b967c4
JoaoGuilhermeMaia <joao.gn.maia99@gmail.com>
2022-12-07 23:03:17 -0300

    Correção da tipagem do response da request

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,46 +3,47 @@ import { Component } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
-
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+	selector: 'app-login',
+	templateUrl: './login.page.html',
+	styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
 
-  httpOptions: object = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" })
-  }
+	httpOptions: object = {
+		headers: new HttpHeaders({ 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" })
+	}
 
-  apiUrl: string = 'http://localhost:8080';
-  email: string = "";
-  password: string = "";
+	apiUrl: string = 'http://181.221.14.79:9003/';
+	email: string = "";
+	password: string = "";
 
-  constructor(private httpClient: HttpClient, private router: Router) {
-  }
+	constructor(private httpClient: HttpClient, private router: Router) {
+	}
 
-  authenticate(){
-     this.httpClient.get(`${this.apiUrl}/user/auth/${this.email}/${this.password}`, this.httpOptions)
-     .pipe(
-       retry(2),
-       catchError(this.handleError)
-      ).subscribe((Response: object) => {
-        this.router.navigate(['/detail']);
-      });
-  }
+	authenticate() {
+		this.httpClient.get(`${this.apiUrl}/user/auth/${this.email}/${this.password}`, this.httpOptions)
+			.pipe(
+				retry(2),
+				catchError(this.handleError)
+			).subscribe((response) => {
+				console.log(response["name"]);
 
-  handleError(error: HttpErrorResponse) {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      errorMessage = error.error.message;
-    } else {
-      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
-    }
-    console.log(errorMessage);
-    return throwError(errorMessage);
-  };
+				this.router.navigate(['/detail']);
+			});
+	}
+
+	handleError(error: HttpErrorResponse) {
+		let errorMessage = '';
+		if (error.error instanceof ErrorEvent) {
+			errorMessage = error.error.message;
+		} else {
+			errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+		}
+		console.log(errorMessage);
+		return throwError(errorMessage);
+	};
 
 }
