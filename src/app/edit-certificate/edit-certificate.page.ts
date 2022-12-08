2a35f5916ba571e84705357da62bfb598bf86864
GuiBertoldi <guilherme_chuia@hotmail.com>
2022-12-08 13:47:56 -0300

    direcionamento de paginas dos botões de voltar

--- src/app/edit-certificate/edit-certificate.page.ts
+++ src/app/edit-certificate/edit-certificate.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCertificatePage implements OnInit {
 
+  amountTime: any | undefined;
+
   constructor() { }
 
   ngOnInit() {
